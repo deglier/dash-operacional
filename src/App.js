839a548c06d887fd87fd285269fd2a8d75b67f85
Deglier da Silva de Amorim <deglier.amorim@neobpo.com.br>
2019-02-13 18:18:46 -0200

    Criação da NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import logo_neo from "./logo-neo.png";
+import logo_gab from "./logo-gab.png";
+import NavBar from "./Components/navbar";
+import { CssBaseline } from "@material-ui/core";
 
 class App extends Component {
   render() {
+    const logosHeader = {
+      cliente: logo_gab,
+      neo: logo_neo
+    };
     return (
       <div className="App">
-        <h1>Hey Hey!</h1>
+        <CssBaseline />
+        <NavBar titulo="Dash Opercional" logos={logosHeader} />
       </div>
     );
   }
