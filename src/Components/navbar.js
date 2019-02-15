03f1083d047cc324532e6fda0b20760a733d0211
Deglier da Silva de Amorim <deglier.amorim@neobpo.com.br>
2019-02-15 19:34:19 -0200

    restruturação de coponentes

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,52 +1,29 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
-import { Grid, CardMedia } from "@material-ui/core";
-
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  logoNeo: {
-    height: "20px",
-    width: "70px",
-    margin: "10px",
-    float: "left"
-  },
-  logoCliente: {
-    height: "20px",
-    width: "130px",
-    margin: "10px",
-    float: "right"
-  },
-  title: {
-    textAlign: "center",
-    paddingTop: "8px"
-  }
-};
-
+import Logo from "./logo";
 
 const NavBar = props => {
-  const { classes, titulo, logos } = props;
-  
+  const { titulo } = props;
   return (
-      <AppBar position="static" color="default">
-        <Grid container spacing={0}>
-          <Grid item xs={1}>
-            <CardMedia image={logos.neo} className={classes.logoNeo} />
-          </Grid>
-          <Grid item xs={10}>
-            <Typography variant="body2" align="center" color="inherit" className={classes.title}>
-              {titulo}
-            </Typography>
-          </Grid>
-          <Grid item xs={1} className={classes.direita}>
-            <CardMedia image={logos.cliente} className={classes.logoCliente} />
-          </Grid>
-        </Grid>
-      </AppBar>
+    <nav className="row mb-2" style={{ backgroundColor: "#fff" }}>
+      <span className="col">
+        <Logo who="neo" color="rgb(15,98,172)" width="75px" height="40px" />
+      </span>
+      <span
+        className="col-9"
+        style={{
+          flexGrow: 1,
+          textAlign: "center",
+          paddingTop: "8px",
+          fontWeight: "bold"
+        }}
+      >
+        <span>{titulo}</span>
+      </span>
+      <span className="col">
+        <Logo who="gab" color="rgb(15,98,172)" width="150px" height="40px" />
+      </span>
+    </nav>
   );
 };
 
-export default withStyles(styles)(NavBar);
+export default NavBar;
