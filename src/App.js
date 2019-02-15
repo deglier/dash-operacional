ca978ab9aa33e4fea6c8539add43b68a325d08b3
Deglier da Silva de Amorim <deglier.amorim@neobpo.com.br>
2019-02-15 19:32:19 -0200

    ESTRUTURAÇÃO DO COMPONENTE "APP"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,96 @@
 import React, { Component } from "react";
-import logo_neo from "./logo-neo.png";
-import logo_gab from "./logo-gab.png";
 import NavBar from "./Components/navbar";
-import { CssBaseline } from "@material-ui/core";
+import Container from "./Components/Container";
+import axios from "axios";
+
+const theads = [
+  <span>ESPECIALIDES</span>,
+  <span>
+    <small>chamadas</small>
+    <br /> RECEBIDAS
+  </span>,
+  <span>
+    <small>chamadas</small>
+    <br /> ABANDONADAS
+  </span>,
+  <span>
+    <small>chamadas</small>
+    <br /> ATENDIDAS
+  </span>,
+  <span>
+    <small>chamadas</small>
+    <br /> ESPERANDO
+  </span>,
+  <span>NS</span>,
+  <span>
+    <small>agentes</small>
+    <br /> LOGADOS
+  </span>,
+  <span>
+    <small>agentes</small>
+    <br /> DISPONIVEIS
+  </span>,
+  <span>
+    <small>agentes</small>
+    <br /> FALANDO
+  </span>,
+  <span>
+    <small>agentes</small>
+    <br /> PAUSA
+  </span>,
+  <span>
+    <small>mais antiga</small> <br /> ESPERANDO
+  </span>,
+  <span>
+    <small>temp méd</small>
+    <br /> ABANDONO
+  </span>
+];
 
 class App extends Component {
-  render() {
-    const logosHeader = {
-      cliente: logo_gab,
-      neo: logo_neo
+  constructor(props) {
+    super(props);
+    this.state = {
+      theads,
+      maisAntiga: 1,
+      isFila: this.fila > 0 ? true : false,
+      fila: 0,
+      updated_at: "",
+      grupos: [],
+      codigos: {},
+      AgtsDisponiveis: 0,
+      AgtsPAUSA: 0
     };
+  }
+  componentWillMount() {
+    this.carregarDados();
+    //console.log("chegando...");
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.carregarDados();
+    }, 10000);
+  }
+
+  carregarDados() {
+    axios.get("/dados.json").then(response =>
+      this.setState({
+        isFila: response.data[0].fila > 0 ? true : false,
+        fila: response.data[0].fila,
+        updated_at: response.data[0].updated_at,
+        grupos: response.data[0].grupos,
+        AgtsDisponiveis: response.data[0].grupos[0].AgtsDisponiveis,
+        AgtsPAUSA: response.data[0].grupos[0].AgtsPAUSA
+      })
+    );
+  }
+
+  render() {
     return (
-      <div className="App">
-        <CssBaseline />
-        <NavBar titulo="Dash Opercional" logos={logosHeader} />
+      <div className="container-fluid" style={{ backgroundColor: "#dedede" }}>
+        <NavBar titulo="Acompanhamento Tempo Real" />
+        <Container {...this.state} />
       </div>
     );
   }
