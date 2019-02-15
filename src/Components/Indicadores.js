db1486d1d481f28f137cafd701f5d4525f0ce8d1
Deglier da Silva de Amorim <deglier.amorim@neobpo.com.br>
2019-02-15 19:34:52 -0200

    add aside panel

---
+++ src/Components/Indicadores.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+const Indicadores = () => {
+  return (
+    <div className="col no-gutters flex-column">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Indicadores;
