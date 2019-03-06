caa2690cdd2c25dd2abc7fee7cea9d81be000ead
Deglier Amorim <degliersa@gmail.com>
2019-03-06 14:32:54 -0300

    implements trigger for change indicators size

--- src/Components/Indicadores.js
+++ src/Components/Indicadores.js
@@ -28,17 +28,42 @@ const Indicadores = props => {
           AgtsFalando={AgtsFalando}
         />
       </Card>
-      <Card>
-        <Indicator indicador="Mais Antiga - Esperando" bigger valor="00:10" />
-      </Card>
-      <div className="row no-gutters">
-        <Card col="6">
-          <Indicator indicador="TMA" valor={utils.tmaGeral(props.grupos)} />
-        </Card>
-        <Card col="6">
-          <Indicator indicador="TME" valor={utils.tmeGeral(props.grupos)} />
-        </Card>
-      </div>
+      {!props.isFila ? (
+        <React.Fragment>
+          <Card>
+            <Indicator
+              indicador="TMA"
+              bigger
+              valor={utils.tmaGeral(props.grupos)}
+            />
+          </Card>
+          <Card>
+            <Indicator
+              indicador="TME"
+              bigger
+              valor={utils.tmeGeral(props.grupos)}
+            />
+          </Card>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Card>
+            <Indicator
+              indicador="Chamada Mais Antiga"
+              bigger
+              valor={utils.gruposByMaisAntiga(props.grupos)}
+            />
+          </Card>
+          <div className="row no-gutters">
+            <Card col="6">
+              <Indicator indicador="TMA" valor={utils.tmaGeral(props.grupos)} />
+            </Card>
+            <Card col="6">
+              <Indicator indicador="TME" valor={utils.tmeGeral(props.grupos)} />
+            </Card>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 };
